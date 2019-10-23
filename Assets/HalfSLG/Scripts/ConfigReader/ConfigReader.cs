2e9baf373aa9627c7781b2c8a117a30d9d01159d
Ty-Chen <369494003@qq.com>
2019-10-23 23:57:23 +0800

    增加XML相关功能
    1. 加入xml配置文件夹Connfig
    2. 增加xml读取函数框架

---
+++ Assets/HalfSLG/Scripts/ConfigReader/ConfigReader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SLGame
+{
+    public class ConfiReader
+    {
+        public static void Init()
+        {
+            Dictionary<uint, SkillConfigInfo> sp = SkillInfoDic;
+        }
+    }
+}
---
+++ Assets/HalfSLG/Scripts/ConfigReader/ReadSkillConfig.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System;
+
+namespace SLGame
+{
+    public class SkillConfigInfo : System.Object
+    {
+        public uint id;
+        public string name;
+        public string icon;
+        public string action;
+        public string effect;
+        public string sound;
+        public string info;
+    }
+
+    class ReadSkillPassiveConfig
+    {
+        XmlDocument xmlDoc = null;
+        public ReadSkillPassiveConfig()
+        {
+        }
+        public ReadSkillPassiveConfig(string xmlFilePath)
+        {
+            //TextAsset xmlfile = Resources.Load(xmlFilePath) as TextAsset;
+            ResourceUnit xmlfileUnit = ResourcesManager.Instance.loadImmediate(xmlFilePath, ResourceType.ASSET);
+            TextAsset xmlfile = xmlfileUnit.Asset as TextAsset;
+
+            if (!xmlfile)
+            {
+                return;
+            }
+
+            xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlfile.text);
+            XmlNodeList infoNodeList = xmlDoc.SelectSingleNode("SkillCfg_passitive ").ChildNodes;
+            for (int i = 0; i < infoNodeList.Count; i++)
+            {//(XmlNode xNode in infoNodeList)
+                if ((infoNodeList[i] as XmlElement).GetAttributeNode("un32ID") == null)
+                    continue;
+
+                string typeName = (infoNodeList[i] as XmlElement).GetAttributeNode("un32ID").InnerText;
+                //Debug.LogError(typeName);
+                SkillConfigInfo passiveInfo = new SkillConfigInfo();
+                passiveInfo.id = (uint)Convert.ToUInt32(typeName);
+                foreach (XmlElement xEle in infoNodeList[i].ChildNodes)
+                {
+                    #region 搜索
+                    switch (xEle.Name)
+                    {
+                        case "szName":
+                            {
+                                passiveInfo.name = Convert.ToString(xEle.InnerText);
+                            }
+                            break;
+
+                        case "SkillIcon":
+                            {
+                                passiveInfo.icon = Convert.ToString(xEle.InnerText);
+                            }
+                            break;
+
+                        case "n32ReleaseAction":
+                            {
+                                passiveInfo.action = Convert.ToString(xEle.InnerText);
+                            }
+                            break;
+
+                        case "n32ReleaseSound":
+                            {
+                                passiveInfo.sound = Convert.ToString(xEle.InnerText);
+                            }
+                            break;
+
+                        case "ReleaseEffect":
+                            {
+                                passiveInfo.effect = Convert.ToString(xEle.InnerText);
+                            }
+                            break;
+
+                        case "info":
+                            passiveInfo.info = Convert.ToString(xEle.InnerText);
+                            break;
+                    }
+
+                    #endregion
+                }
+                ConfigReader.skillPassiveInfoDic.Add(passiveInfo.id, passiveInfo);
+                //Debug.LogError("add buff" + buffInfo.BuffID);
+            }
+        }
+    }
+}
+
+
