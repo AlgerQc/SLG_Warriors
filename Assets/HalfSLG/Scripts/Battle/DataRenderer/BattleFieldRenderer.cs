cd22e0d6d4367676379a91147e191dc07ecc6ac4
Ty-Chen <369494003@qq.com>
2019-11-17 21:54:12 +0800

    增加了鼠标悬停触发逻辑
    目前悬停触发逻辑并未成功触发，可能原因在于上层UI遮蔽了效果

--- Assets/HalfSLG/Scripts/Battle/DataRenderer/BattleFieldRenderer.cs
+++ Assets/HalfSLG/Scripts/Battle/DataRenderer/BattleFieldRenderer.cs
@@ -14,6 +14,7 @@ using UnityEngine.EventSystems;
 namespace SLGame
 {
     using Resource;
+    using UnityEngine.Events;
 
     public class BattleFieldRenderer
         : MonoBehaviourSingleton<BattleFieldRenderer>,
--- Assets/HalfSLG/Scripts/Battle/DataRenderer/BattleUnitRenderer.cs
+++ Assets/HalfSLG/Scripts/Battle/DataRenderer/BattleUnitRenderer.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 namespace SLGame
 {
@@ -61,7 +64,35 @@ namespace SLGame
             frameRenderer.sortingLayerID = EGameConstL.SortingLayer_Battle_Map;
             battleUnitInfo.sortingLayerID = EGameConstL.SortingLayer_Battle_Map;
             battleUnitHealthBar.Init();
+            AddTriggersListener(unitRenderer.gameObject, EventTriggerType.PointerEnter, showUI);
         }
+
+        
+        private void showUI(BaseEventData data)
+        {
+            UtilityHelper.Log("We are enter in battle unit and wait for operation");
+        }
+
+        private void AddTriggersListener(GameObject obj, EventTriggerType eventID, UnityAction<BaseEventData> action)
+        {
+            EventTrigger trigger = obj.GetComponent<EventTrigger>();
+            if (trigger == null)
+            {
+                trigger = obj.AddComponent<EventTrigger>();
+            }
+
+            if (trigger.triggers.Count == 0)
+            {
+                trigger.triggers = new List<EventTrigger.Entry>();
+            }
+
+            UnityAction<BaseEventData> callback = new UnityAction<BaseEventData>(action);
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = eventID;
+            entry.callback.AddListener(callback);
+            trigger.triggers.Add(entry);
+        }
+        
 
         //刷新值显示
         private void RefreshAttribute(BattleUnitSyncAttribute attribute)
